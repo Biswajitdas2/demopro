0dbe74ed7fc1f58a9826226ec644c83b8824effa
Biswajit <161626779+Biswajitdas2@users.noreply.github.com>
2024-05-29 22:25:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+gsap.from("nav a",{
+    y:20,
+    duration:1.5,
+    stagger:0.6,
+    opacity:0
+})
+gsap.from("h1",{
+    y:-20,
+    duration:1.5,
+    stagger:0.6,
+    opacity:0,
+    delay:0.8
+})
+
+gsap.from(".text-section h3",{
+    y:-20,
+    duration:1.5,
+    stagger:0.6,
+    opacity:0
+})
+
+gsap.from(".cv",{
+    y:-20,
+    duration:1.5,
+    stagger:0.6,
+    opacity:0
+})
+
+gsap.from(".contact",{
+    x:-20,
+    duration:1.5,
+    stagger:0.6,
+    opacity:0
+})
+
+gsap.from(".icons i",{
+    x:20,
+    duration:1.5,
+    stagger:0.6,
+    opacity:0
+})
+gsap.from(".photo-section",{
+    duration:1.5,
+    stagger:0.6,
+    opacity:0
+})
+
